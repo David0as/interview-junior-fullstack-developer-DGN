5e6fadf66e84e4f04601ff2cb5128b024fe747c9
david.gebelli <david.geben.123@gmail.com>
2023-08-03 06:47:54 +0200

    Frontend donne. Basic stiling too

--- interview-frontend/src/app/pipefilter.pipe.ts
+++ interview-frontend/src/app/pipefilter.pipe.ts
@@ -8,12 +8,12 @@ export class PipefilterPipe implements PipeTransform {
   transform(cities: any[], searchText: string): any[] {
     if(!cities) return [];
     if(!searchText) return [];
-    if (searchText == "") return cities;
+    
 
     searchText = searchText.toLowerCase();
 
     return cities.filter( city => {
-      return city.toLowerCase().includes(searchText);
+      return city.cityName.toLowerCase().includes(searchText);
     })
   }
 
